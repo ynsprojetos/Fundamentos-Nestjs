9db509b6eb6444402d6e1d68fd292eaff7508019
Yuri Silva <ynscontato@gmail.com>
2021-11-21 13:27:34 -0300

    TRABALHANDO COM PARÂMETROS EM ROTAS

--- devtraining/src/courses/courses.controller.ts
+++ devtraining/src/courses/courses.controller.ts
@@ -1,9 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('courses')
 export class CoursesController {
     @Get('list')
-    findAll(){
-        return 'Listagem de cursos'  
+    findAll() {
+        return 'Listagem de cursos'
     }
+
+    @Get(':id')
+    findoOne(@Param('id') id: string) {
+        return `CURSO #${id}`;
+    }
+
+
 }
