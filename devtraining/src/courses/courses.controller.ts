1861d6c79f141f201b353f1976680079503cd4a4
ynsprojetos <ynscontato@gmail.com>
2021-12-05 15:14:04 -0300

    CONHECENDO E CRIANDO UM SERVIÇO COM NESTJS

--- devtraining/src/courses/courses.controller.ts
+++ devtraining/src/courses/courses.controller.ts
@@ -1,13 +1,15 @@
+import { CourseService } from './../../../course/course.service';
 import { Controller, Get, Param } from '@nestjs/common';
 @Controller('courses')
 export class CoursesController {
+    constructor(private readonly courseService: CourseService) { }
     @Get()
-    findAll(){
-        return 'Listagem de cursos'  
+    findAll() {
+        return 'Listagem de cursos'
     }
 
     @Get(':id')
-    findOne(@Param() params){
-        return `Curso #${params.id}` 
+    findOne(@Param() params) {
+        return `Curso #${params.id}`
     }
 }
