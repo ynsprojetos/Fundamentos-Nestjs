fa2f1a6587e08252b65181e175186a1ba7551f76
Yuri Silva <ynscontato@gmail.com>
2021-12-25 20:37:51 -0300

    TYPEORM - BUSCANDO DADOS RELACIONADOS

--- devtraining/src/courses/courses.service.ts
+++ devtraining/src/courses/courses.service.ts
@@ -14,11 +14,13 @@ export class CoursesService {
     ) { }
 
     findAll() {
-        return this.courseRepository.find()
+        return this.courseRepository.find({
+            relations: ['tags']
+        })
     }
 
     findOne(id: string) {
-        const course = this.courseRepository.findOne(id);
+        const course = this.courseRepository.findOne(id, { relations: ['tags'] });
 
         if (!course) {
             throw new NotFoundException(`Course ID ${id} not found`)
