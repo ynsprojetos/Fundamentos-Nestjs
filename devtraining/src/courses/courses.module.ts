2928d26e02a9b6fb5770a654c3de6e8cb2e6d701
Yuri Silva <ynscontato@gmail.com>
2021-12-25 10:26:33 -0300

    TYPEORM - CONFIGURANDO A ENTIDADE COURSE

--- devtraining/src/courses/courses.module.ts
+++ devtraining/src/courses/courses.module.ts
@@ -1,8 +1,11 @@
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { CoursesService } from './courses.service';
 import { CoursesController } from './courses.controller';
 import { Module } from '@nestjs/common';
+import { Course } from './entities/course.entity';
 
 @Module({
+    imports: [TypeOrmModule.forFeature([Course])],
     controllers: [CoursesController],
     providers: [CoursesService]
 })
